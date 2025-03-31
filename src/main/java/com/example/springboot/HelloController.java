bb132c7b18fb7132c5e2cb902d4929cfcd079e69
jinjingchi <reed0017.chi@gmail.com>
2025-03-31 21:42:25 +0800

    first commit

---
+++ src/main/java/com/example/springboot/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
